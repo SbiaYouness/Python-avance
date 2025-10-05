a59d1aab9cd107637c72f1793596b85b1bf09dd0
lamprofony <lamprofony@outlook.com>
2025-10-05 13:28:01 +0100

    atelier 1

---
+++ atelier 1/act6.py
@@ -0,0 +1,13 @@
+# #q1
+# for i in range(1, 6, 1):
+#     for j in range(0, i, 1):
+#         print(i, end=" ")
+#     print("")
+
+#q2
+k=1
+for i in range(1, 5, 1):
+    for j in range(0, i, 1):
+        print(k, end=" ")
+        k+=1
+    print("")
